6b878562d4d13264cb933f2139fb84ee2b114a2e
in6802 <in6802@gmail.com>
2018-11-13 15:41:54 +0900

    design

--- app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
+++ app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.edu.imageview_gallery_capture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button buttonGallery, buttonCapture;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //design link
+        buttonGallery = findViewById(R.id.buttonGallery);
+        buttonGallery.setOnClickListener(this);
+        buttonCapture = findViewById(R.id.buttonCapture);
+        buttonCapture.setOnClickListener(this);
+        imageView = findViewById(R.id.imageView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }

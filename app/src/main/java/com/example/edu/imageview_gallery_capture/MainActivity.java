3b65c16d3e383d03d8f85c654373d1042a312563
in6802 <in6802@gmail.com>
2018-11-13 15:51:26 +0900

    Gallery

--- app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
+++ app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.edu.imageview_gallery_capture;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.view.View;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button buttonGallery, buttonCapture;
     ImageView imageView;
+    int LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +37,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, LOAD_IMAGE);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(data != null){
+            Uri seletedImage = data.getData();
+            InputStream inputStream = null;
+            try {
+                inputStream = this.getContentResolver().openInputStream(seletedImage);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Log.e ("inputStream", "FileNotFoundException");
+
+            }
+
+        }
+        /*switch(LOAD_IMAGE){
+            case 1:
+                break;
+            case 2:
+                break;
+        }*/
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }

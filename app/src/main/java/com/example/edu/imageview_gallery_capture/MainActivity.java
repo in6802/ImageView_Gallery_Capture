6cf68b2fb74ab14bd104f998c0f8029c4c694494
in6802 <in6802@gmail.com>
2018-11-13 16:55:13 +0900

    합치기

--- app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
+++ app/src/main/java/com/example/edu/imageview_gallery_capture/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.edu.imageview_gallery_capture;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,7 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button buttonGallery, buttonCapture;
     ImageView imageView;
-    int LOAD_IMAGE = 1;
+    final int LOAD_IMAGE = 1;
+    final int IMAGE_CAPTURE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,40 +38,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, LOAD_IMAGE);
-
-
+        switch(view.getId()){
+            case R.id.buttonGallery:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, LOAD_IMAGE);
+                break;
+            case R.id.buttonCapture:
+                if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+                    Intent intent1 = new Intent();
+                    intent1.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent1, IMAGE_CAPTURE);
+                }
+                break;
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        int a = 1;
+        switch(requestCode) {
+            case LOAD_IMAGE:
+                if (data != null) {
+                    Uri seletedImage = data.getData();
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = this.getContentResolver().openInputStream(seletedImage);
 
-        if(data != null){
-            Uri seletedImage = data.getData();
-            InputStream inputStream = null;
-            try {
-                inputStream = this.getContentResolver().openInputStream(seletedImage);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        //decodeStream 일렬로 가지고 오는 비트를 재배치
+                        imageView.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                        Log.e("inputStream", "FileNotFoundException");
+                    }
+                }
+                break;
 
-                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                imageView.setImageBitmap(bitmap);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Log.e ("inputStream", "FileNotFoundException");
-
-            }
-
+            case IMAGE_CAPTURE:
+                if(requestCode == IMAGE_CAPTURE && resultCode == RESULT_OK){
+                    Bundle extras = data.getExtras();
+                    Bitmap bitmap = (Bitmap)extras.get("data");
+                    imageView.setImageBitmap(bitmap);
+                }
+                break;
         }
-        /*switch(LOAD_IMAGE){
-            case 1:
-                break;
-            case 2:
-                break;
-        }*/
         super.onActivityResult(requestCode, resultCode, data);
     }
 }
